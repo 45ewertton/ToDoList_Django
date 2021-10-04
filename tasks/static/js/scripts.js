8c7394c8f0ed2a7d330e794ac1508283b637befd
45ewertton <ewertton45mb@gmail.com>
2021-10-04 16:41:12 -0300

    Script do botao de delete

--- tasks/static/js/scripts.js
+++ tasks/static/js/scripts.js
@@ -1,1 +1,18 @@
-console.log('Funcionou!')
+(function (win, doc){
+    'use strict';
+
+    //Verificar se o usuário realmente quer deletar o dado...
+    if(doc.querySelector('.delete-btn')){
+        let btnDel = doc.querySelectorAll('.delete-btn');
+        for (let i=0; i < btnDel.length; i++){
+            btnDel[i].addEventListener('click', function(event){
+                if(confirm('Deseja mesmo apagar este dado?')){
+                    return true;
+
+                }else{
+                    event.preventDefault();
+                } 
+            })
+        }
+    }
+}) (window, document)
