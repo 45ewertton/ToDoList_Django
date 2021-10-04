764584f2d332c7ba941eb635363e096c79a5693e
45ewertton <ewertton45mb@gmail.com>
2021-10-04 15:09:47 -0300

    Update do crud de tasks realizado e tambem algumas correcoes

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import taskList, yourName, taskView, newTask
+from .views import taskList, yourName, taskView, newTask, editTask
 
 urlpatterns = [
     path('', taskList, name='task-list'),
     path('task/<int:id>', taskView, name="task-view"),
     path('newTask/', newTask, name="new-task"),
+    path('editTask/<int:id>', editTask, name="edit-task"),
 
 
     path('yourname/<str:name>', yourName, name='your-name'),
