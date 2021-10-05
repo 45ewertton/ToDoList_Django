17a611dda3c7f376915685b54e0106c110520808
45ewertton <ewertton45mb@gmail.com>
2021-10-05 14:30:09 -0300

    ForeignKey entre User e Task

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 STATUS = (
@@ -14,6 +14,7 @@ class Task(models.Model):
         max_length=5,
         choices=STATUS, 
     )
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
