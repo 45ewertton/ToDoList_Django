e74ca5572f291ab5987303fff46c0108343c1675
45ewertton <ewertton45mb@gmail.com>
2021-10-05 15:07:33 -0300

    Last commit

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import taskList, yourName, taskView, newTask, editTask, deleteTask
+from .views import taskList, yourName, taskView, newTask, editTask, deleteTask, changeStatus
 
 urlpatterns = [
     path('', taskList, name='task-list'),
     path('task/<int:id>', taskView, name="task-view"),
     path('newTask/', newTask, name="new-task"),
     path('editTask/<int:id>', editTask, name="edit-task"),
+    path('changeStatus/<int:id>', changeStatus, name="change-status"),
     path('deleteTask/<int:id>', deleteTask, name="delete-task"),
 
 
